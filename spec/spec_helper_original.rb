1ea3ca7e6f627dd5f7a0054448a355acdbd5deae
roshini <roshini@freshdesk.com>
2014-01-09 11:28:04 +0530

    added a full_title helper

--- spec/spec_helper_original.rb
+++ spec/spec_helper_original.rb
@@ -73,7 +73,6 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-config.mock_with :rspec
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
